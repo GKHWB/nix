71701819459ac7d03608eeff05b930c7000bb696
GKHWB <kingdomg@tuta.com>
2025-03-31 09:25:08 -0400

    fish config

--- cradle/home/shells/fish.nix
+++ cradle/home/shells/fish.nix
@@ -4,6 +4,7 @@
     functions = {
       vim = "nvim $argv";
       rebuild = "sudo nixos-rebuild switch --flake /home/alter/nix/#cradle && swww img -t grow /home/alter/nix/cradle/wallpapersymlink"; 
+      tm = "tmux attach";
     };
   };
 }
