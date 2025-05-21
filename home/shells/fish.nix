1452b6e30040a938fbf7e26ec3662d14b6f45081
GKHWB <kingdomg@tuta.com>
2025-05-21 11:00:42 -0400

    config

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -7,6 +7,7 @@
       vim = "nvim $argv";
       mv = "command mv -i $argv";
       rebuild = "sudo nixos-rebuild switch --flake /home/alter/nix/#${osConfig.networking.hostName}"; 
+      update = "sudo nix flake update --flake /home/alter/nix/";
       tm = "tmux attach";
     };
   };
