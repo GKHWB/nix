9d63d7a599e61a973444ff801399931fcb9d5070
GKHWB <kingdomg@tuta.com>
2025-05-21 15:14:58 -0400

    waybar battery module only for laptop

--- home/wm/waybar.nix
+++ home/wm/waybar.nix
@@ -1,13 +1,25 @@
+{ osConfig, ...}:
+
 {
 	programs.waybar = {
 		enable = true;
-		settings = {
+		settings = if osConfig.networking.hostName == "amalgam" then
+		{
 			mainBar = {
 				layer = "top";
 				position = "top";
 				modules-left = [ "niri/workspaces" ];
-				modules-center = [ "hyprland/window" ];
 				modules-right = [ "battery" "clock" "tray" ];
+				spacing = 5;
+			};
+		}
+		else
+		{
+			mainBar = {
+				layer = "top";
+				position = "top";
+				modules-left = [ "niri/workspaces" ];
+				modules-right = [ "clock" "tray" ];
 				spacing = 5;
 			};
 		};
