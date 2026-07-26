5500fce7908b456a12f87fc9e22700d0885ab387
GKHWB <kingdomg@tuta.com>
2026-07-26 17:24:10 -0400

    asylum: enable experimental features

--- modules/hosts/asylum/configuration.nix
+++ modules/hosts/asylum/configuration.nix
@@ -8,6 +8,8 @@
         self.modules.nixos.alter
     ];
 
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
     # Bootloader.
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/sdb";
