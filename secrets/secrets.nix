74b2fa48d20ed43fe8d8180d9899a47609a67bb3
GKHWB <kingdomg@tuta.com>
2026-08-03 00:48:45 -0400

    allow cradle to edit wireguard-endpoint.age without root

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   cradle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDComyuKhWoZsOmRyn+MgyTJ5zJjDpyl2VzASL681qtV";
+  cradleAlter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbJ7Nq1zJ8a2Av4lQkzt3F7nxsANtdHRLRDheHC97t7";
   amalgam = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoris0Y3YWDETaG4ivlOMNmB0Wr9aIe24Qt3auG8iYO";
 in
 {
-  "wireguard-endpoint.age".publicKeys = [ cradle amalgam ];
+  "wireguard-endpoint.age".publicKeys = [ cradle cradleAlter amalgam ];
 }
 
